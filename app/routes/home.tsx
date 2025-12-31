c6a65dbd451b61c4a8bba03e843cccfad61601d5
True-Land <ethelusmar@outlook.com>
2025-12-31 15:11:27 -0500

    Modified home tsx to showcase components

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,10 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import Header from "../Header/header";
+import "../Header/header.css";
+
+import Content from "../Content_Section/Content_Section";
+import "../Content_Section/Content_Section.css";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -7,7 +12,18 @@ export function meta({}: Route.MetaArgs) {
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
+const tabs = ["Home","Spotlight","Tourism","Resources"];
+
+
 
 export default function Home() {
-  return <Welcome />;
+  var h = <Header title="SunShine Central" tabs={tabs} />;
+ 
+  return <div> {h} <Welcome /> 
+  <Content title={tabs[0]}/> 
+  <Content title={tabs[1]}/>  
+  <Content title={tabs[2]}/> 
+  <Content title={tabs[3]}/>  
+
+  </div>;
 }
