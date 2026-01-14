9482b60075757c5ad24345bf2dd2e978a72e02e1
motionlikewo <lloydpeyton48@gmail.com>
2026-01-14 17:37:05 -0500

    Main Menu design and Routing Work

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,18 +1,18 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
-import Header from "../Header/header";
-import "../Header/header.css";
+import Header from "../pages/main-menu/Header/header";
+import "../pages/main-menu/Header/header.css";
+import "../pages/main-menu/main-menu.css";
 
-import Content from "../Content_Preview/Content_Preview";
-import "../Content_Preview/Content_Preview.css";
+import Content from "../pages/main-menu/Content_Preview/Content_Preview";
+import "../pages/main-menu/Content_Preview/Content_Preview.css";
 
 import "../Image_Slider/Image_Slider.css";
 import ImageSlider from "../Image_Slider/Image_Slider"; 
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Sunshine Central" },
+    { name: "description", content: "Welcome to Sunshine Central!" },
   ];
 }
 const tabs = ["Home","Spotlight","Tourism","Resources"];
@@ -22,7 +22,7 @@ import i3 from "../Image_Slider/Test_Images/US.png";
 export default function Home() {
   var h = <Header title="SunShine Central" tabs={tabs} />;
  
-  return <div> {h} <Welcome /> 
+  return <div> {h}
   <Content title={tabs[0]}/> 
   <Content title={tabs[1]}/>  
   <Content title={tabs[2]}/> 
