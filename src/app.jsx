c898295949a1367c770032a656cad4868eb0bf5c
motionlikewo <lloydpeyton48@gmail.com>
2026-02-26 13:19:54 -0500

    Created basic submit form and submitting feature.

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import SubmitPage from './Pages/submit/submit.jsx';
 
 function App() {
   return (
     <div className="app-wrapper" style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh'}}>
-      <Navbar />
       <main style={{ flex: 1 }}>
         <Routes>
-          /*Define your routes here*/
+          <Route path="/submit" element={<SubmitPage />} />
         </Routes>
       </main>
-      <Footer />
     </div>
   );
 }
