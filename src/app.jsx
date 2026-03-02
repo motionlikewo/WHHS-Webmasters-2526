0d5d97770afaead048ecb3de5e047bd4c15a8a2e
motionlikewo <lloydpeyton48@gmail.com>
2026-03-02 13:55:54 -0500

    Created placeholder.

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import About from './Pages/about/about.jsx';
 
 function App() {
   return (
     <div className="app-wrapper" style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh'}}>
-      <Navbar />
       <main style={{ flex: 1 }}>
         <Routes>
-          /*Define your routes here*/
+          <Route path="/about" element={<About />} />
         </Routes>
       </main>
-      <Footer />
     </div>
   );
 }
