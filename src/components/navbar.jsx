b91ff80e3bda68a4f49a0ea3c9493a2f92e3c7b4
motionlikewo <lloydpeyton48@gmail.com>
2026-03-02 17:58:24 -0500

    Routing work

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import './navbar.css';
-import logo from '../assets/sunshineCentrallogo.png'; 
+import logo from '../assets/sunshineCentralLogo.png'; 
 
 const Navbar = () => {
   return (
